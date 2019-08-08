670e7b557652a5524ec3ea86a57abef605855a34
kabundege <christophekwizera1@gmail.com>
2019-08-08 12:49:47 -0700

    feat(bookAtrip):delivers the API endpoint codes to book a trip
    -the API first will check if the user if authorized to make that that
    action
    -the API will chech the data the user entered to book are valid
    -the API will will send a success massage.
    
    [delivers #167781112]

---
+++ server/server/middleware/trips_validation.js
@@ -0,0 +1,26 @@
+const Joi = require('@hapi/joi');
+const today = new Date();
+const year = today.getFullYear();
+
+
+
+function validateUser(req,res,next){
+const schema ={
+    bus_license_number: Joi.string().alphanum().min(7).max(7).required(),
+    origin: Joi.string().alphanum().min(4).max(30).required(),
+    destination: Joi.string().alphanum().min(4).max(30).required(),
+    seating_capacity: Joi.number().integer().max(100).required(),
+    trip_date: Joi.number().integer().min(year).required(),
+    fare: Joi.number().required(),
+    status: Joi.string().min(3).max(5),
+}
+const {error} = Joi.validate(req.body, schema);
+    if (error) {
+        return res.status(400).json({
+            status:'400',
+            error:error.details[0].message
+        });
+    }
+    next();
+}
+export default validateUser;
